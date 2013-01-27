ebff556ee6d17dfd6756226bcbd0f0807a8a085b
Mikhail <provizionm@gmail.com>
2013-01-27 15:36:02 +0400

    Buttons are more beautiful

--- Dali AlfaRelease/RootViewController.m
+++ Dali AlfaRelease/RootViewController.m
@@ -44,7 +44,7 @@
 
     // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
     CGRect pageViewRect = self.view.bounds;
-    pageViewRect = CGRectInset(pageViewRect, 40.0, 40.0);
+    pageViewRect = CGRectInset(pageViewRect, 0, 0);
     self.pageViewController.view.frame = pageViewRect;
 
     [self.pageViewController didMoveToParentViewController:self];
